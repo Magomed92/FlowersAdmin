5793f87b183d4bcef477d51924b4ba6caabcabce
Magomed92 <apach.2014@mail.ru>
2022-05-14 16:40:54 +0300

    init-project

---
+++ src/pages/Flowers/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminLayout from "../../layouts/AdminLayout";
+
+const Flowers = () => {
+  return (
+    <div>
+      <AdminLayout>Flowers</AdminLayout>
+    </div>
+  );
+};
+
+export default Flowers;
